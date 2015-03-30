1f0acf90ca247a5f4561ea386cb79dacef5777e7
Anthony Gomez <gomeza08@gmail.com>
2015-03-30 16:11:58 -0400

    added blank angualr app

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sudokuSolverAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sudokuSolverAppApp
+ */
+angular.module('sudokuSolverAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sudokuSolverAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sudokuSolverAppApp
+ */
+angular.module('sudokuSolverAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
